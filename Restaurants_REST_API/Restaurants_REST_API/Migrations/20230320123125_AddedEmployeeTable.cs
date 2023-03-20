780e886f392e58ddef4e3d081d7df51cb8502665
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-03-20 13:33:45 +0100

    Added employee table to rest api, also migration has been added

---
+++ Restaurants_REST_API/Restaurants_REST_API/Migrations/20230320123125_AddedEmployeeTable.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Restaurants_REST_API.Migrations
+{
+    public partial class AddedEmployeeTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    IdEmployee = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Salary = table.Column<decimal>(type: "money", nullable: false),
+                    HiredDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsHealthBook = table.Column<string>(type: "nvarchar(1)", maxLength: 1, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    PESEL = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    IdAddress = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.IdEmployee);
+                    table.ForeignKey(
+                        name: "FK_Employees_Address_IdAddress",
+                        column: x => x.IdAddress,
+                        principalTable: "Address",
+                        principalColumn: "IdAddress",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_IdAddress",
+                table: "Employees",
+                column: "IdAddress");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
