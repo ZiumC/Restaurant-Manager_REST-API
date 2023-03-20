fa867d3ae4d0ac0200c7261feb632c5cd28476a7
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-03-20 11:28:52 +0100

    Added models folder, Address model has been added

---
+++ Restaurants_REST_API/Restaurants_REST_API/Models/Address.cs
@@ -0,0 +1,11 @@
+﻿namespace Restaurants_REST_API.Models
+{
+    public class Address
+    {
+        public int IdAddress { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string NoBuilding { get; set; }
+        public string NoLocal { get; set; }
+    }
+}
