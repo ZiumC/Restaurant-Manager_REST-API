1be9609f81921a24f8c6b0ca60db4caf6f007c82
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-03-20 13:19:23 +0100

    Added client and reserwations tables to rest api, also migration has been added

---
+++ Restaurants_REST_API/Restaurants_REST_API/Models/Reservation.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net;
+
+namespace Restaurants_REST_API.Models
+{
+    public class Reservation
+    {
+        [Key]
+        public int IdReservation { get; set; }
+
+        [Required]
+        public DateTime ReservationDate { get; set; }
+
+        [Required]
+        public int TableNumber { get; set; }
+
+
+        public int IdClient { get; set; }
+
+        [ForeignKey(nameof(IdClient))]
+        public virtual Client Clients { get; set; }
+
+
+        public int IdRestauration { get; set; }
+
+        [ForeignKey(nameof(IdRestauration))]
+        public virtual Restaurant Restaurant { get; set; }
+    }
+}
