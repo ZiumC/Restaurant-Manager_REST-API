d1961ae6ad61d989686338f941b096d087749d5c
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-03-20 16:59:02 +0100

    Added Employees and Restaurants Controllers. Also added services for those controllers

---
+++ Restaurants_REST_API/Restaurants_REST_API/Services/Database Service/IEmployeeApiService.cs
@@ -0,0 +1,14 @@
+﻿using Restaurants_REST_API.Models;
+
+namespace Restaurants_REST_API.Services.Database_Service
+{
+    public interface IEmployeeApiService
+    {
+        public Task<IEnumerable<Employee>> GetAllEmployeesAsync();
+        public Task<Employee> GetEmployeeByIdAsync(int empId);
+        public Task<IEnumerable<Employee>> GetAllSupervisorsAsync();
+        public Task<Employee> GetSupervisorByIdAsync(int empId);
+        public Task<Employee> GetOwnerAsync();
+        public Task<IEnumerable<Employee>> GetAllEmployeesByRestaurantIdAsync(int restaurantId);
+    }
+}
