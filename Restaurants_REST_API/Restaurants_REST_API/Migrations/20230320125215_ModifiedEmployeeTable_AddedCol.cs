655299f99a21beab7b8b71790e6ee56d210d49fb
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-03-20 14:01:28 +0100

    Added modified employee table

---
+++ Restaurants_REST_API/Restaurants_REST_API/Migrations/20230320125215_ModifiedEmployeeTable_AddedCol.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Restaurants_REST_API.Migrations
+{
+    public partial class ModifiedEmployeeTable_AddedCol : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IsOwner",
+                table: "Employees",
+                type: "nvarchar(1)",
+                maxLength: 1,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsOwner",
+                table: "Employees");
+        }
+    }
+}
