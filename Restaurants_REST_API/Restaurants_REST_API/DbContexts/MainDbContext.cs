be4f6df133dcae5b6817a36046715a317c04b874
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-03-20 12:29:07 +0100

    Added connection string, dbcontext and migrations

---
+++ Restaurants_REST_API/Restaurants_REST_API/DbContexts/MainDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurants_REST_API.Models;
+
+namespace Restaurants_REST_API.DbContexts
+{
+    public class MainDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        public MainDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public MainDbContext(DbContextOptions opt) : base(opt)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(_configuration["ConnectionStrings:Default"]);
+            Console.WriteLine(_configuration["ConnectionStrings:Default"]);
+        }
+
+        public DbSet<Address> Address { get; set; }
+
+
+    }
+}
