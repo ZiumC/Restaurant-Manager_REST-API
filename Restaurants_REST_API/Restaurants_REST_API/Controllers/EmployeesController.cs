679c27f1743ebd312fd71bccad7f27d285f1cff6
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-03-20 19:19:03 +0100

    Added EmployeeDTO, also created endpoint for all employees

--- Restaurants_REST_API/Restaurants_REST_API/Controllers/EmployeesController.cs
+++ Restaurants_REST_API/Restaurants_REST_API/Controllers/EmployeesController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Restaurants_REST_API.Models;
+using Restaurants_REST_API.Services.Database_Service;
 
 namespace Restaurants_REST_API.Controllers
 {
@@ -7,6 +9,25 @@ namespace Restaurants_REST_API.Controllers
     [ApiController]
     public class EmployeesController : ControllerBase
     {
+        private readonly IEmployeeApiService _employeeApiService;
+
+        public EmployeesController(IEmployeeApiService employeeApiService)
+        {
+            _employeeApiService = employeeApiService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            var employees = await _employeeApiService.GetAllEmployeesAsync();
+
+            if (employees == null)
+            {
+                return NotFound("Employees not found");
+            }
+
+            return Ok(employees);
+        }
 
     }
 }
--- Restaurants_REST_API/Restaurants_REST_API/Program.cs
+++ Restaurants_REST_API/Restaurants_REST_API/Program.cs
@@ -1,13 +1,16 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Restaurants_REST_API.DbContexts;
+using Restaurants_REST_API.Services.Database_Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddScoped<IEmployeeApiService, EmployeeApiService>();
+builder.Services.AddSingleton<IRestaurantApiService, RestaurantApiService>();
+builder.Services.AddDbContext<MainDbContext>(opt => opt.UseSqlServer("name=ConnectionStrings:Default"));
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+ //Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
