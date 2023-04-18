0815a6700359026bf1eaaf88bd42d761c1d0278f
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-04-18 11:11:23 +0200

    Employee validation has been added

--- Restaurants_REST_API/Restaurants_REST_API/DTOs/AddressDTO.cs
+++ Restaurants_REST_API/Restaurants_REST_API/DTOs/AddressDTO.cs
@@ -1,11 +1,24 @@
-﻿namespace Restaurants_REST_API.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Restaurants_REST_API.DTOs
 {
     public class AddressDTO
     {
         public int IdAddress { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [MinLength(5)]
         public string City { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [MinLength(5)]
         public string Street { get; set; }
+        [Required]
+        [MaxLength(5)]
+        [MinLength(1)]
         public string BuildingNumber { get; set; }
+        [MaxLength(5)]
+        [MinLength(1)]
         public string? LocalNumber { get; set; }
     }
 }
--- Restaurants_REST_API/Restaurants_REST_API/DTOs/EmployeeDTO.cs
+++ Restaurants_REST_API/Restaurants_REST_API/DTOs/EmployeeDTO.cs
@@ -5,13 +5,25 @@ namespace Restaurants_REST_API.DTOs
     public class EmployeeDTO
     {
         public int IdEmployee { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [MinLength(3)]
         public string FirstName { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [MinLength(3)]
         public string Surname { get; set; }
+        [Required]
+        [MaxLength(11)]
+        [MinLength(11)]
         public string PESEL { get; set; }
+        [Required]
         public decimal Salary { get; set; }
         public DateTime HiredDate { get; set; }
         public DateTime? FirstPromotionChefDate { get; set; }
+        [Required]
         public string IsOwner { get; set; }
+        [Required]
         public AddressDTO Address { get; set; }
         public List<CertificateDTO>? Certificates { get; set;}
     }
