5de2246732789ff65c5d853cc8e12c01f32a9fe2
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-04-25 08:55:07 +0200

    Working on client controller

--- Restaurants_REST_API/Restaurants_REST_API/DTOs/ReservationDTO.cs
+++ Restaurants_REST_API/Restaurants_REST_API/DTOs/ReservationDTO.cs
@@ -1,11 +1,15 @@
-﻿namespace Restaurants_REST_API.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Restaurants_REST_API.DTOs
 {
     public class ReservationDTO
     {
         public int IdReservation { get; set; }
+        [Required]
         public DateTime ReservationDate { get; set; }
         public string Status { get; set; }
         public int? ReservationGrade { get; set; }
+        [Required]
         public int TableNumber { get; set; }
         public ComplainDTO? ReservationComplain { get; set; }
     }
