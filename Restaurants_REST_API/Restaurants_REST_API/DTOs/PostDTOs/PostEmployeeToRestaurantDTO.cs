9c474e8d09c652232b6b2d677dd377f1f5d8c088
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-04-25 17:10:22 +0200

    Added PostEmployeeToRestaurantDTO class and also removed unused GetEmployeeHiredDTO

--- Restaurants_REST_API/Restaurants_REST_API/DTOs/PostDTOs/PostEmployeeToRestaurantDTO.cs
+++ Restaurants_REST_API/Restaurants_REST_API/DTOs/PostDTOs/PostEmployeeToRestaurantDTO.cs
@@ -1,8 +1,8 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace Restaurants_REST_API.DTOs.GetDTOs
+namespace Restaurants_REST_API.DTOs.PostDTOs
 {
-    public class GetEmployeeHiredDTO
+    public class PostEmployeeToRestaurantDTO
     {
         [Required]
         public int IdEmployee { get; set; }
