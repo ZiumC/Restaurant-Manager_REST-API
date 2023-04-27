db74d83b22d07c46c489a40c63bd75453fb68dfa
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-04-27 09:49:50 +0200

    created put DTOs, renamed api link

---
+++ Restaurants_REST_API/Restaurants_REST_API/DTOs/PutDTOs/PutCertificateDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Restaurants_REST_API.DTOs.PutDTOs
+{
+    public class PutCertificateDTO
+    {
+        [MaxLength(125)]
+        [MinLength(5)]
+        public string Name { get; set; }
+        public DateTime ExpirationDate { get; set; }
+    }
+}
