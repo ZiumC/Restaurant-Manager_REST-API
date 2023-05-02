d267737ceee36b63e6cce2136af92caa403ad405
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-05-02 14:55:46 +0200

    Added validation to endpoint and also added mapper

--- Restaurants_REST_API/Restaurants_REST_API/Services/MapperService/MapEmployeeCertificatesService.cs
+++ Restaurants_REST_API/Restaurants_REST_API/Services/MapperService/MapEmployeeCertificatesService.cs
@@ -1,6 +1,47 @@
-﻿namespace Restaurants_REST_API.Services.MapperService
+﻿using Restaurants_REST_API.DTOs.GetDTOs;
+using Restaurants_REST_API.DTOs.PutDTO;
+
+namespace Restaurants_REST_API.Services.MapperService
 {
     public class MapEmployeeCertificatesService
     {
+        private readonly GetEmployeeDTO _employeeDetailsDatabase;
+        private readonly IEnumerable<PutCertificateDTO> _certificatesData;
+        private List<string> updatedCertificateNames = new List<string>();
+
+        public MapEmployeeCertificatesService(GetEmployeeDTO employeeDetailsDatabase, IEnumerable<PutCertificateDTO> certificatesData)
+        {
+            _employeeDetailsDatabase = employeeDetailsDatabase;
+            _certificatesData = certificatesData;
+        }
+
+        private void UpdateEmployeeCertificates()
+        {
+            List<int> updatedCertificatesId = new List<int>();
+
+            if (_employeeDetailsDatabase.Certificates == null)
+            {
+                throw new Exception("Employee doesn't have any certificates");
+            }
+
+            for (int i = 0; i < _employeeDetailsDatabase.Certificates.Count(); i++)
+            {
+                string oldName = _employeeDetailsDatabase.Certificates.ElementAt(i).Name.ToLower();
+                string newName = _certificatesData.ElementAt(i).Name.ToLower();
+
+                if (!oldName.Equals(newName))
+                {
+                    updatedCertificateNames.Add(newName);
+                    updatedCertificatesId.Add(_employeeDetailsDatabase.Certificates.ElementAt(i).IdCertificate);
+                }
+            }
+
+        }
+
+        public List<string> GetEmployeeUpdatedCertificates()
+        {
+            UpdateEmployeeCertificates();
+            return updatedCertificateNames;
+        }
     }
 }
