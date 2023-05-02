feb2f6cae1e56771d3db5b1e3b22cd21319b5051
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-05-02 14:21:46 +0200

    Renamed some pices of code and added PutEmployeeDTO

---
+++ Restaurants_REST_API/Restaurants_REST_API/DTOs/PutDTO/PutCertificateDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Restaurants_REST_API.DTOs.PutDTO
+{
+    public class PutCertificateDTO
+    {
+        [Required]
+        [MaxLength(125)]
+        [MinLength(5)]
+        public string Name { get; set; }
+        [Required]
+        public DateTime ExpirationDate { get; set; }
+    }
+}
