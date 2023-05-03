58c392a3cdb351cfb93467211e757901b440d912
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-05-03 19:58:04 +0200

    Added PutDishDto, empty dish mapper and validation in put endpoint

---
+++ Restaurants_REST_API/Restaurants_REST_API/DTOs/PutDTO/PutDishDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Restaurants_REST_API.DTOs.PutDTO
+{
+    public class PutDishDTO
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
