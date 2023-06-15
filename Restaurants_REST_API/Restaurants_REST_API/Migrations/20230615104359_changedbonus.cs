5308b1f5eb8c5b66cdf669a30205ace71adae693
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-06-15 15:43:26 +0200

    Small changes in db contex

---
+++ Restaurants_REST_API/Restaurants_REST_API/Migrations/20230615104359_changedbonus.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Restaurants_REST_API.Migrations
+{
+    public partial class changedbonus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Restaurant",
+                keyColumn: "IdRestaurant",
+                keyValue: 1,
+                column: "BonusBudget",
+                value: 1230m);
+
+            migrationBuilder.UpdateData(
+                table: "Restaurant",
+                keyColumn: "IdRestaurant",
+                keyValue: 2,
+                column: "BonusBudget",
+                value: 400m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Restaurant",
+                keyColumn: "IdRestaurant",
+                keyValue: 1,
+                column: "BonusBudget",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Restaurant",
+                keyColumn: "IdRestaurant",
+                keyValue: 2,
+                column: "BonusBudget",
+                value: null);
+        }
+    }
+}
