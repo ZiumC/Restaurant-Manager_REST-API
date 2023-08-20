1074d0a7e19189b944e406ae16f3f96edfb52050
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-08-20 17:41:20 +0200

    Endpoint for getting client, reservations and complaint data has been added

--- Restaurants_REST_API/Restaurants_REST_API/Controllers/ComplaintsController.cs
+++ Restaurants_REST_API/Restaurants_REST_API/Controllers/ComplaintsController.cs
@@ -1,8 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Restaurants_REST_API.DTOs.GetDTOs;
-using Restaurants_REST_API.Models.Database;
 using Restaurants_REST_API.Services.Database_Service;
-using Restaurants_REST_API.Services.ValidatorService;
+using Restaurants_REST_API.Services.DatabaseService.CustomersService;
 
 namespace Restaurants_REST_API.Controllers
 {
@@ -11,13 +10,72 @@ namespace Restaurants_REST_API.Controllers
     public class ComplaintsController : ControllerBase
     {
         private readonly IRestaurantApiService _restaurantsApiService;
-        private readonly IComplaintApiService _reservationsApiService;
+        private readonly IClientApiService _clientApiService;
+        private readonly IComplaintApiService _complaintsApiService;
+        private readonly IConfiguration _config;
 
-        public ComplaintsController(IRestaurantApiService restaurantsApiService, IComplaintApiService reservationsApiService)
+        public ComplaintsController(IRestaurantApiService restaurantsApiService, IComplaintApiService complaintsApiService, IClientApiService clientApiService, IConfiguration config)
         {
             _restaurantsApiService = restaurantsApiService;
-            _reservationsApiService = reservationsApiService;
+            _clientApiService = clientApiService;
+            _complaintsApiService = complaintsApiService;
+            _config = config;
         }
 
+        [HttpGet]
+        public async Task<IActionResult> GetComplainsByStatus(string status)
+        {
+            IEnumerable<string> aviableStatus = new List<string>
+            {
+                _config["ApplicationSettings:ComplaintStatus:New"],
+                _config["ApplicationSettings:ComplaintStatus:Pending"],
+                _config["ApplicationSettings:ComplaintStatus:Accepted"],
+                _config["ApplicationSettings:ComplaintStatus:Cancelled"]
+            };
+
+            if (!aviableStatus.Contains(status))
+            {
+                return BadRequest("Complaint status is invalid");
+            }
+
+            var rawComplaints = await _complaintsApiService.GetClientComplaintsByStatusAsync(status);
+            if (rawComplaints == null || rawComplaints.Count() == 0)
+            {
+                return NotFound("Complains not found");
+            }
+
+            rawComplaints.RemoveAll(rc => rc.ClientReservations == null || rc.ClientReservations.Count() == 0);
+            List<GetClientDataDTO>? filteredComplaints = rawComplaints;
+            if (filteredComplaints == null || filteredComplaints.Count() == 0)
+            {
+                return NotFound("Complains not found");
+            }
+
+            var restaurantsDetails = await _restaurantsApiService.GetAllRestaurantsAsync();
+            if (restaurantsDetails == null || restaurantsDetails.Count() == 0)
+            {
+                return NotFound("Restaurants not found");
+            }
+
+            var result = rawComplaints.Select(c => new
+            {
+                ClientName = c.Name,
+                IsBusinessMan = c.IsBusinessman,
+                ClientReservations = c.ClientReservations?.Select(cr => new
+                {
+                    ResevtaionDate = cr.ReservationDate,
+                    ReservationStatus = cr.Status,
+                    ReservationGrade = cr.ReservationGrade,
+                    HowManyPeoples = cr.HowManyPeoples,
+                    RestaurantName = restaurantsDetails
+                    .Where(rd => rd.RestaurantReservations
+                    .Any(a => a.IdReservation == cr.IdReservation))
+                    .FirstOrDefault()?.Name,
+                    Complaint = cr.ReservationComplaint
+                })
+            });
+
+            return Ok(result);
+        }
     }
 }
