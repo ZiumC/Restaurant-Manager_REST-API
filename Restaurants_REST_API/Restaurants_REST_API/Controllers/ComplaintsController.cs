228fcf75bff0996a29d134f3e1d85bd75c69a038
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-08-20 13:11:40 +0200

    Initial commit for this branch

---
+++ Restaurants_REST_API/Restaurants_REST_API/Controllers/ComplaintsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Restaurants_REST_API.DTOs.GetDTOs;
+using Restaurants_REST_API.Models.Database;
+using Restaurants_REST_API.Services.Database_Service;
+using Restaurants_REST_API.Services.ValidatorService;
+
+namespace Restaurants_REST_API.Controllers
+{
+    [Route("api/manage/[controller]")]
+    [ApiController]
+    public class ComplaintsController : ControllerBase
+    {
+        private readonly IRestaurantApiService _restaurantsApiService;
+        private readonly IComplaintApiService _reservationsApiService;
+
+        public ComplaintsController(IRestaurantApiService restaurantsApiService, IComplaintApiService reservationsApiService)
+        {
+            _restaurantsApiService = restaurantsApiService;
+            _reservationsApiService = reservationsApiService;
+        }
+
+    }
+}
---
+++ Restaurants_REST_API/Restaurants_REST_API/Services/DatabaseService/ReservationsService/IComplaintApiService.cs
@@ -0,0 +1,12 @@
+﻿using Restaurants_REST_API.DTOs.GetDTOs;
+
+namespace Restaurants_REST_API.Services.Database_Service
+{
+    public interface IComplaintApiService
+    {
+        public Task<List<GetComplaintDTO>?> GetNewComplaintsAsync();
+        public Task<List<GetComplaintDTO>?> GetPendingComplaintsAsync();
+        public Task<GetComplaintDTO?> GetComplaintDetailsByComplaintIdAsync(int complaintId);
+        public Task<bool> UpdateComplaintStatusByComplaintId(int complaint, string status);
+    }
+}
