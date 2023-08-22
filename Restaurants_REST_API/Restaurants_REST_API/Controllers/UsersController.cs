34f800d3c71b61f42060e710f34c23777ef06b2f
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-08-22 18:18:46 +0200

    Added validation for register endpoint

--- Restaurants_REST_API/Restaurants_REST_API/Controllers/UsersController.cs
+++ Restaurants_REST_API/Restaurants_REST_API/Controllers/UsersController.cs
@@ -1,8 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Restaurants_REST_API.DTOs.PostDTO;
 using Restaurants_REST_API.Services.Database_Service;
 using Restaurants_REST_API.Services.DatabaseService.CustomersService;
+using System.Text.RegularExpressions;
 
 namespace Restaurants_REST_API.Controllers
 {
@@ -14,7 +14,7 @@ namespace Restaurants_REST_API.Controllers
         private readonly IEmployeeApiService _employeeApiService;
         private readonly IConfiguration _config;
 
-        UsersController(IClientApiService clientApiService, IEmployeeApiService employeeApiService, IConfiguration config)
+        public UsersController(IClientApiService clientApiService, IEmployeeApiService employeeApiService, IConfiguration config)
         {
             _clientApiService = clientApiService;
             _employeeApiService = employeeApiService;
@@ -22,8 +22,36 @@ namespace Restaurants_REST_API.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<IActionResult> RegisterNewUser(PostUserDTO newUser) 
+        public async Task<IActionResult> RegisterNewUser(PostUserDTO newUser)
         {
+            if (newUser == null)
+            {
+                return BadRequest("Data can't be null");
+            }
+
+            string loginRegex = _config["ApplicationSettings:DataValidation:LoginRegex"];
+            string emailRegex = _config["ApplicationSettings:DataValidation:EmailRegex"];
+
+            if (!Regex.Match(newUser.Login, loginRegex, RegexOptions.IgnoreCase).Success)
+            {
+                return BadRequest("Login is invalid");
+            }
+
+            if (!Regex.Match(newUser.Email, emailRegex, RegexOptions.IgnoreCase).Success)
+            {
+                return BadRequest("Email is invalid");
+            }
+
+            string? pesel = newUser.PESEL;
+            if (!string.IsNullOrEmpty(pesel))
+            {
+                string peselRegex = _config["ApplicationSettings:DataValidation:PeselRegex"];
+                if (pesel.Count() != 11 || !Regex.Match(pesel, peselRegex, RegexOptions.IgnoreCase).Success)
+                {
+                    return BadRequest("PESEL is invalid");
+                }
+            }
+
             return Ok();
         }
     }
