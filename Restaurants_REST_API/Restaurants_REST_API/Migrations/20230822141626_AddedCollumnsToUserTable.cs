f1d120ffb924f8fc309527ea799959692cbb9d0b
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-08-22 16:18:21 +0200

    Initial commit for this branch

---
+++ Restaurants_REST_API/Restaurants_REST_API/Migrations/20230822141626_AddedCollumnsToUserTable.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Restaurants_REST_API.Migrations
+{
+    public partial class AddedCollumnsToUserTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateBlockedTo",
+                table: "User",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "User",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Login",
+                table: "User",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "User",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Login",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "User");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateBlockedTo",
+                table: "User",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+    }
+}
