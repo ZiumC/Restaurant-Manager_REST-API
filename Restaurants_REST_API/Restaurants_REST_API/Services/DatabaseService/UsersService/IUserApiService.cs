48b1a01d06bb1e41b456bda5b58938e56ab308da
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-08-22 18:18:46 +0200

    Added validation for register endpoint

--- Restaurants_REST_API/Restaurants_REST_API/Services/DatabaseService/UsersService/IUserApiService.cs
+++ Restaurants_REST_API/Restaurants_REST_API/Services/DatabaseService/UsersService/IUserApiService.cs
@@ -1,6 +1,10 @@
-﻿namespace Restaurants_REST_API.Services.DatabaseService.UsersService
+﻿using Restaurants_REST_API.DTOs.PostDTO;
+
+namespace Restaurants_REST_API.Services.DatabaseService.UsersService
 {
     public interface IUserApiService
     {
+        public Task<bool> RegisterNewClientAsync(PostUserDTO newUser);
+        public Task<bool> RegisterNewEmployeeAsync(PostUserDTO newUser);
     }
 }
--- Restaurants_REST_API/Restaurants_REST_API/Services/DatabaseService/UsersService/UserApiService.cs
+++ Restaurants_REST_API/Restaurants_REST_API/Services/DatabaseService/UsersService/UserApiService.cs
@@ -1,6 +1,17 @@
-﻿namespace Restaurants_REST_API.Services.DatabaseService.UsersService
+﻿using Restaurants_REST_API.DTOs.PostDTO;
+
+namespace Restaurants_REST_API.Services.DatabaseService.UsersService
 {
     public class UserApiService : IUserApiService
     {
+        public Task<bool> RegisterNewClientAsync(PostUserDTO newUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RegisterNewEmployeeAsync(PostUserDTO newUser)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
