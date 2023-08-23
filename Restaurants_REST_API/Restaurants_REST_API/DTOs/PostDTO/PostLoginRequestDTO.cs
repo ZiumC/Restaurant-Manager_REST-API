179c1dd86156cc892239aac53bfbef7564b88433
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-08-23 18:00:13 +0200

    Added DTO and created empty endpoint for login

---
+++ Restaurants_REST_API/Restaurants_REST_API/DTOs/PostDTO/PostLoginRequestDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Restaurants_REST_API.DTOs.PostDTO
+{
+    public class PostLoginRequestDTO
+    {
+        [Required]
+        [MaxLength(50)]
+        [MinLength(3)]
+        public string LoginOrEmail { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [MinLength(3)]
+        public string Password { get; set; }
+    }
+}
