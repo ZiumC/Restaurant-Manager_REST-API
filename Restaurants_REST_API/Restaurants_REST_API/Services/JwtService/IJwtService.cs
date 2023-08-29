766494d89a338069f119923e0f9d417e7e35064c
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-08-29 16:44:30 +0200

    Added comments to methods

--- Restaurants_REST_API/Restaurants_REST_API/Services/JwtService/IJwtService.cs
+++ Restaurants_REST_API/Restaurants_REST_API/Services/JwtService/IJwtService.cs
@@ -5,8 +5,24 @@ namespace Restaurants_REST_API.Services.JwtService
 {
     public interface IJwtService
     {
+        /// <summary>
+        /// Method generate refresh token. Length of token depends on given length in appsettings.json 
+        /// </summary>
+        /// <returns>Refresh token.</returns>
         public string GenerateRefreshToken();
+
+        /// <summary>
+        /// Method generate access token for user. Token contains user claims and token is signed by server.
+        /// </summary>
+        /// <param name="user">User data</param>
+        /// <returns>Signed access token.</returns>
         public string GenerateAccessTokenForUser(User user);
+
+        /// <summary>
+        /// Method valids passed pair of access and refresh token.
+        /// </summary>
+        /// <param name="jwt">Pair of access token and refresh token.</param>
+        /// <returns>True if token is valid or false  when token isn't valid.</returns>
         public bool ValidateJwt(PostJwtDTO jwt);
     }
 }
