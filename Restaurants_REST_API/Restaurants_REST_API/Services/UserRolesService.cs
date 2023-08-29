a1c98ef4544bba7f1fd3d1ba9c12f65b96dadd7e
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-08-29 18:46:57 +0200

    Added authorization to employees controller and also changed swagger to read bearer token

---
+++ Restaurants_REST_API/Restaurants_REST_API/Services/UserRolesService.cs
@@ -0,0 +1,14 @@
+﻿namespace Restaurants_REST_API.Services
+{
+    public static class UserRolesService
+    {
+        public const string Owner = "OWNER";
+        public const string Supervisor = "SUPERVISOR";
+        public const string OwnerAndSupervisor = $"{Owner},{Supervisor}";
+
+        public const string Employee = "EMPLOYEE";
+        public const string Client = "CLIENT";
+        public const string EmployeeAndClient = $"{Employee},{Client}";
+
+    }
+}
