03590e038e7deddc65301d60e005b4b7193c4e31
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-09-01 12:49:12 +0200

    Removed unnecessary code

--- Restaurants_REST_API/Restaurants_REST_API/Services/DatabaseService/UsersService/IUserApiService.cs
+++ Restaurants_REST_API/Restaurants_REST_API/Services/DatabaseService/UsersService/IUserApiService.cs
@@ -7,7 +7,6 @@ namespace Restaurants_REST_API.Services.DatabaseService.UsersService
         public Task<bool> RegisterNewClientAsync(User registerClient);
         public Task<bool> RegisterNewEmployeeAsync(User registerEmployee);
         public Task<User?> GetUserDataByEmpId(int empId);
-        public Task<User?> GetUserDataByEmail(string email);
         public Task<User?> GetUserDataByLoginOrEmail(string loginOrEmil);
         public Task<User?> GetUserDataByRefreshToken(string refreshToken);
         public Task<bool> UpdateUserData(User userData);
