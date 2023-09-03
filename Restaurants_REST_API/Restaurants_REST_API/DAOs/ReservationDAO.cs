82537b5c9d50e0bf76cfd8b7b6bbc4b7cf011766
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-09-03 14:46:35 +0200

    changed some methods to dao

---
+++ Restaurants_REST_API/Restaurants_REST_API/DAOs/ReservationDAO.cs
@@ -0,0 +1,9 @@
+﻿namespace Restaurants_REST_API.DAOs
+{
+    public class ReservationDAO
+    {
+        public DateTime ReservationDate { get; set; }
+        public int HowManyPeoples { get; set; }
+        public int IdRestaurant { get; set; }
+    }
+}
--- Restaurants_REST_API/Restaurants_REST_API/Services/DatabaseService/ClientsService/IClientApiService.cs
+++ Restaurants_REST_API/Restaurants_REST_API/Services/DatabaseService/ClientsService/IClientApiService.cs
@@ -1,4 +1,5 @@
-﻿using Restaurants_REST_API.DTOs.GetDTOs;
+﻿using Restaurants_REST_API.DAOs;
+using Restaurants_REST_API.DTOs.GetDTOs;
 using Restaurants_REST_API.DTOs.PostDTO;
 
 namespace Restaurants_REST_API.Services.DatabaseService.CustomersService
@@ -8,8 +9,9 @@ namespace Restaurants_REST_API.Services.DatabaseService.CustomersService
         public Task<GetClientDataDTO?> GetClientDetailedDataByIdAsync(int clientId); 
         public Task<IEnumerable<GetReservationDTO>?> GetAllReservationsDetailedDataByClientIdAsync(int clientId);
         public Task<GetReservationDTO?> GetReservationDetailedDataByCliennIdReservationIdAsync(int clientId, int reservationId);
-        public Task<bool> MakeReservationByClientIdAsync(int clientId, PostReservationDTO newReservation);
-        public Task<bool> UpdateReservationByClientIdAsync(int clientId, GetReservationDTO reservation);
+        public Task<bool> MakeReservationByClientIdAsync(int clientId, ReservationDAO newReservation);
+        public Task<bool> UpdateReservationStatusAsync(int clientId, int reservationId, string status);
+        public Task<bool> UpdateReservationGradeAsync(int clientId, int reservationId, int grade);
         public Task<bool> MakeComplainByClientIdAsync(int clientId, GetReservationDTO reservation, GetComplaintDTO newComplaint);
     }
 }
